7f9263ecd1ed74597740e18d66038a5f71fd3d6b
kuba_cichan <kuba.cichan@gmail.com>
2014-01-12 23:31:46 +0100

    Naprawiona klasa Input. Teraz czyta jak powinna.

--- src/pszty2/Input.java
+++ src/pszty2/Input.java
@@ -1,63 +1,37 @@
 package pszty2;
 
-import java.io.BufferedReader;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 import java.util.Vector;
 
-public class Input {
+public class Input
+{
+
 	private Vector<Vector<Double>> data;
-	
-	short rows, cols;
-	short current_row;
-	short[] p;
+	int currentRow;
+	int[] p;
 
-	/**
-	 * 
-	 * @param fname
-	 * @param row_num
-	 * @param col_num
-	 * @throws FileNotFoundException
-	 * @throws IOException
-	 */
-	
-	public void LoadDataFromFile(String fname)
-			throws FileNotFoundException, IOException {
-		BufferedReader br = new BufferedReader(new FileReader(fname));
 
-		String line;
-
-		Scanner sc;
-		
-		data = new Vector<Vector<Double>>();
-		
-		while((line = br.readLine()) != null) 
-		{
-			sc = new Scanner(line);
-			
-			Vector<Double> temp = new Vector<Double>();
-
-			while(sc.hasNextDouble())
-			{
-				temp.add(sc.nextDouble());
-				
-				if(rows == 0)
-					cols++;
-			}
-			
-			data.add(temp);
-			
-			sc.close();
-			
-			rows++;
-		}
-
-		br.close();
-	}
+    public Input(String fname) throws FileNotFoundException
+    {
+        data = new Vector<Vector<Double>>();
+        Scanner sc = new Scanner( new File( fname ) );
+        while( sc.hasNextLine() )
+        {
+            data.add( new Vector<Double>() );
+            String s = sc.nextLine();
+            Scanner temp = new Scanner( s );
+            while( temp.hasNext() )
+            {
+                data.get(currentRow).add( Double.parseDouble( temp.next() ) );
+            }
+            ++currentRow;
+        }
+        currentRow = 0;
+        sc.close();
+    }
 	
 	/**
 	 * 
@@ -66,23 +40,23 @@ public class Input {
 	 */
 	public List<Double> getNextInput() throws IOException
 	{		
-		if(current_row >= rows)
+		if(currentRow >= data.size())
 			throw new IOException("End of rows");
 	
 		List<Double> ret = new ArrayList<Double>();
 		
-		for(short i = 0; i < p.length; ++i)
+		for(int i = 0; i < p.length; ++i)
 			if(p[i] == 1)
-				ret.add(data.get(current_row).get(i));
+				ret.add(data.get(currentRow).get(i));
 		
-		current_row++;
+		++currentRow;
 		
 		return ret;
 	}
 	
 	public void goToFirst()
 	{
-		current_row = 0;
+		currentRow = 0;
 	}
 	
 	/**
@@ -90,11 +64,8 @@ public class Input {
 	 * @param pattern
 	 * @throws Exception
 	 */
-	public void applyPattern(short[] pattern) throws Exception
+	public void applyPattern(int[] pattern)
 	{
-		if(pattern.length > cols)
-			throw new Exception("Format większy od liczby kolumn");
-		
 		p = pattern;
 	}
 }
