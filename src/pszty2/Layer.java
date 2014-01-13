af64d7709310704d8049f666e86b01b6637fba11
kuba_cichan <kuba.cichan@gmail.com>
2014-01-13 22:48:01 +0100

    Trochę zmieniłem. Pewnie na gorsze, ale jak już się zamotałem to nie chciało mi się wracać do początku, a jak działa to nie chce mi ię zmieniać:P

--- src/pszty2/Layer.java
+++ src/pszty2/Layer.java
@@ -9,128 +9,61 @@ import java.util.List;
 
 public class Layer
  {
-    private Layer nextLayer;
-    public void setNextLayer(Layer l)
-    {
-        nextLayer  = l;
-    }
-
-    private Layer previousLayer;
-    public void setPreviousLayer(Layer l)
-    {
-        previousLayer = l;
-    }
-
-    private Double output;
-    public Double getOutput()
-    {
-        return output;
-    }
-     /**
-      * Type of neurons in layer, could be hidden layer or output layer.
-      */
-    private LAYER_TYPE LT;
-
-     /**
+	 /**
       * List of nodes contained in specific layer
       */
-    private List<Node> nodeList;
+    private ArrayList<Neutron> neutrons;
 
-
-     /**
+	 /**
       * List of raw input from previous layer's output. When calculating nodes output, this has to be multiplied by weights.
       */
-    private List<Double> inputList;
-
-
-     /**
-      * Variable needed when initialising nodes. Nodes need to have specified number of input weights.
-      */
-    private int inputCount;
-
-
-     /**
-      *  Needed when initialising nodes. May be useful later.
-      */
-    private int noOfNodes;
+    private ArrayList<Double> input;
 
 
      /**
       * Public layer constructor initialising an array of nodes, throws IllegalArgumentException when inputCount out of range.
-      * @param inputCount Number of nodes from previous layer - which equals to output count from previous layer. Has to be greater than 0.
-      * @param noOfNodes Number of nodes created in this layer.
+      * @param numberOfInputs Number of nodes from previous layer - which equals to output count from previous layer. Has to be greater than 0.
+      * @param numberOfNeutrons Number of nodes created in this layer.
       * @exception java.lang.IllegalArgumentException
       */
-    public Layer(int inputCount, int noOfNodes, LAYER_TYPE LT)
-    {
-        this.inputCount = inputCount;
-        this.noOfNodes = noOfNodes;
-        this.LT = LT;
-        if(inputCount <= 0) throw new IllegalArgumentException();
-        nodeList = new ArrayList<Node>();
-        inputList = new ArrayList<Double>();
-        for(int i=0; i < noOfNodes; i++)
+    public Layer(int numberOfInputs, int numberOfNeutrons)
+	{
+        neutrons = new ArrayList<Neutron>();
+        input = new ArrayList<Double>();
+        for(int i=0; i < numberOfNeutrons; i++)
         {
-            nodeList.add(new Node(inputCount));
+            neutrons.add(new Neutron(numberOfInputs));
         }
 
     }
 
+	public void calculate()
+	{
+		for( int i = 0; i < neutrons.size(); ++i )
+		{
+			double sum = 0;
+			for ( int j = 0; j < neutrons.get( i ).getWeights().size(); ++j )
+			{
+				sum += neutrons.get( i ).getWeights().get( j ) * input.get( j ); //apply input * weight
+			}
+			sum += neutrons.get( i ).getBias();
 
+			neutrons.get( i ).setOutput( 1/(1+Math.exp( -sum )) );
+		}
+	}
 
-     /**
-      * This imports input list calculated in previous layer. This function should be called by a previous layer after successful output calculation.
-      * @param listToImport List of previous layer's output. It's size must be equal to input count provided in constructor parameter inputCount.
-      * @exception  java.lang.IllegalArgumentException
-      *
-      */
-     public void importInputList(List<Double> listToImport)
-     {
-         if(listToImport.size() != inputCount) throw new IllegalArgumentException("imported size list does not match input count of current layer");
+	public void loadInput(ArrayList<Double> input)
+	{
+		 this.input = input;
+	}
 
-         inputList.clear();
-         inputList.addAll(listToImport);
+	 public ArrayList<Neutron> getNeutrons()
+	 {
+		 return neutrons;
+	 }
 
-     }
-
-     /**
-      * This function calculates the layer output. It calls calculateX methods on each of its neurons(nodes) and combines it into an output vector and updates next layers input.
-      */
-     public void calculateOutput()
-     {
-         Iterator<Node> it = nodeList.iterator();
-         List<Double> l = new ArrayList<Double>();
-
-         if(LT == LAYER_TYPE.HIDDEN_LAYER)
-         {
-            while(it.hasNext())
-            {
-                 l.add(it.next().calculateHiddenOutput(inputList));
-            }
-
-            if(nextLayer != null)
-                nextLayer.importInputList(l);
-
-         }
-         else if(LT == LAYER_TYPE.OUTPUT_LAYER)
-         {
-             while(it.hasNext())
-             {
-                 l.add(it.next().calculateYOutput(inputList));
-                 output = l.get(0);
-             }
-
-         }
-
-
-
-     }
-
-
-
+	 public ArrayList<Double> getInput()
+	 {
+		 return input;
+	 }
  }
-
-enum LAYER_TYPE
-{
-    HIDDEN_LAYER, OUTPUT_LAYER
-}
